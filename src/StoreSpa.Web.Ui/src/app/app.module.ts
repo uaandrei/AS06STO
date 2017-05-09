ec9ab16e6281a030127c44ead5c05b318b9618f6
Ursta <Andrei_Ursta@EMEA.DELL.COM>
2017-05-09 15:02:54 +0300

    a bit of tinkering with the main app

--- src/StoreSpa.Web.Ui/src/app/app.module.ts
+++ src/StoreSpa.Web.Ui/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { StoreAppMainComponent } from "./store-app-main.component";
+
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ StoreAppMainComponent ],
+  bootstrap:    [ StoreAppMainComponent ]
 })
 export class AppModule { }
---
+++ src/StoreSpa.Web.Ui/src/app/store-app-main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'store-app-main',
+  templateUrl: `./store-app-main.component.html`,
+  styleUrls: [`./store-app-main.component.css`],
+})
+export class StoreAppMainComponent  { name = 'Angular'; }
