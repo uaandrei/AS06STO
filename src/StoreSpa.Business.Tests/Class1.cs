cd9360de85ba497f1881ee4b461fd7c364b8e216
Ursta <Andrei_Ursta@EMEA.DELL.COM>
2017-05-18 11:42:35 +0300

    added test project

---
+++ src/StoreSpa.Business.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StoreSpa.Business.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(3, 5);
+        }
+    }
+}
