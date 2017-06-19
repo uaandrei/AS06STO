e618825016051ae9079ee45a68c224733f02e4bc
Andrei, Ursta <ua.andrei@gmail.com>
2017-06-19 17:43:15 +0300

    separated view model and view

--- src/StoreSpa.Web.Ui/src/app/nav/store-app-nav.component.ts
+++ src/StoreSpa.Web.Ui/src/app/nav/store-app-nav.component.ts
@@ -2,28 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'store-app-nav',
-  template: `
-    <nav class="navbar navbar-fixed-top navbar-inverse">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
-                    aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-                <a class="navbar-brand" routerLink="/">Spa Store</a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li routerLinkActive="active"><a routerLink="/home">Home</a></li>
-                    <li routerLinkActive="active"><a routerLink="/list">List</a></li>
-                    <li routerLinkActive="active"><a routerLink="/new">New</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-  `
+  templateUrl: './store-app-nav.component.html'
 })
 export class StoreAppNavComponent  { }
